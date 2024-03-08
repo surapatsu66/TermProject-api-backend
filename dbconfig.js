8a2113e7aafeb9bface414c5af5e9a2489dd1a6b
surapatsu66 <surapat_su66@live.rmutl.ac.th>
2024-03-08 15:05:54 +0700

    dbedit

--- dbconfig.js
+++ dbconfig.js
@@ -11,9 +11,9 @@ var dbconfig = {
         //connectionLimit : 10,
         host     : 'localhost',
         port     : '3306',
-        user     : 'dbuser',
-        password : 'P@ssw0rd@2023',
-        database : 'term_project'
+        user     : 'dbuser66',
+        password : 'P@ssw0rd',
+        database : 'user66033_db'
     }
     };
 module.exports = dbconfig;
